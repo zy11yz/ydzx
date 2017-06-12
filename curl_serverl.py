7b680a63562031e4d1ec98e675018c4808c8286e
zy1211yz <13241222918@163.com>
2017-06-12 14:29:33 +0800

    curl脚本

---
+++ curl_serverl.py
@@ -0,0 +1,20 @@
+import os
+
+'''
+1、先查from_id 
+api：http://dataplatform.yidian.com:8083/api/other/channel_query/query_all
+方法：post
+参数：word
+2、再请求uid
+方法：get
+参数：cs_fromid_、cs_keyword_、type
+'''
+
+word = input('请输入要查的词(以空格分割)：').split(' ')
+path = '/data/home/lidong1/liuyichao/script/azkaban_dim_user/tmp'
+for keyword in word:
+    from_id = os.popen("curl -s -d 'word=%s' 'http://dataplatform.yidian.com:8083/api/other/channel_query/query_all'" % word).read().split('-')[0]
+    curl = "curl -s http://dataplatform.yidian.com:8083/api/other/uids/query_all\?word\=cs_keyword_%s,cs_fromid_%s\&type\=or > %s/%s.txt;" % (keyword, from_id, path, keyword)
+    with open('/data/home/lidong1/User_id_list/script/curl.txt') as c:
+        c.write(curl)
+    os.system(curl)
