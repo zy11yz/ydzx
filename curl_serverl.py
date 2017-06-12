12684c366cd1f332e3c03112ce7b8a763c16c3df
zy1211yz <13241222918@163.com>
2017-06-12 16:29:28 +0800

    curl脚本

--- curl_serverl.py
+++ curl_serverl.py
@@ -11,7 +11,7 @@ api：http://dataplatform.yidian.com:8083/api/other/channel_query/query_all
 '''
 
 word = input('请输入要查的词(以空格分割)：').split(' ')
-path = '/data/home/lidong1/liuyichao/script/azkaban_dim_user/tmp'
+path = input('请输入你要存储的路径')
 for keyword in word:
     from_id = os.popen("curl -s -d 'word=%s' 'http://dataplatform.yidian.com:8083/api/other/channel_query/query_all'" % word).read().split('-')[0]
     curl = "curl -s http://dataplatform.yidian.com:8083/api/other/uids/query_all\?word\=cs_keyword_%s,cs_fromid_%s\&type\=or > %s/%s.txt;" % (keyword, from_id, path, keyword)
